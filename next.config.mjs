d85f7ff58777bc044fd900f8d5cc876091993d72
Erik Hough <erikhough03@gmail.com>
2024-09-13 16:43:44 -0500

    trying to get vercel deployment working

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/beq2peq", // Replace with your actual GitHub repository name
   images: {
-    unoptimized: true, // Disable image optimization for static export
+    unoptimized: true, // This is optional, only if you need unoptimized images
   },
 };
 
